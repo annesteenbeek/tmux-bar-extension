21060ff18553445b095cfa0715e43c540d565add
annesteenbeek <annesteenbeek@gmailcom>
2019-05-22 13:01:07 +0200

    fixed spaces break

--- scripts/load_avg.sh
+++ scripts/load_avg.sh
@@ -8,7 +8,7 @@ update_period=5
 update() {
 	cat << _EOF_ > $tmpfile
 LAST_TS=$(date +%s)
-LOAD_AVG=$(cat /proc/loadavg | awk '{ printf "%.2f %.2f %.2f\n", $1, $2, $3 }')
+LOAD_AVG="$(cat /proc/loadavg | awk '{ printf "%.2f %.2f %.2f", $1, $2, $3 }')"
 _EOF_
 }
 
@@ -22,5 +22,5 @@ else
 fi
 
 source $tmpfile
-printf $LOAD_AVG
+echo $LOAD_AVG
 
